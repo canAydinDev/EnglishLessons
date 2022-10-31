c7108d9965046eeadb9312d13c69c447e94c33a1
canaydin263 <canaydin263@gmail.com>
2022-10-31 20:42:07 +0300

    guncelleme

---
+++ src/main/java/day19stringbuilderandaccessmodifiers/StringBuffer01.java
@@ -0,0 +1,15 @@
+package day19stringbuilderandaccessmodifiers;
+
+public class StringBuffer01 {
+    public static void main(String[] args) {
+
+        /*
+        StringBuffer is another Class and way to create a String in java
+        StringBuffer is very similar with StringBuilder Class, I mean both are mutable
+
+        What is the difference between StringBuffer and StringBuilder?
+        1)StringBuffer was created before StringBuilder in java
+        2)StringBuffer is "multi-thread", but StringBuilder is not.
+         */
+    }
+}
